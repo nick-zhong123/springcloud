74cea41ef4ad74fc73c383d02912150a4cb9368f
nick-zhong123 <zhong_wei_cup@163.com>
2021-12-11 17:11:45 +0800

    nacos配置

--- nacos-client-demo/src/main/java/com/nick/micro/eureka/client/NacosClientDemoApplication.java
+++ nacos-client-demo/src/main/java/com/nick/micro/eureka/client/NacosClientDemoApplication.java
@@ -11,10 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaClientDemoApplication {
+public class NacosClientDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientDemoApplication.class, args);
+        SpringApplication.run(NacosClientDemoApplication.class, args);
     }
 
 }
